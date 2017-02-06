9c32cea409bd774927de791a85885ac9b8d1387f
Tammy Abbett <tammylabbett@gmail.com>
2017-02-06 17:52:42 -0600

    work in progress

---
+++ main.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python
+#
+# Copyright 2007 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+import webapp2
+import re
+
+form = """
+<form method = "post">
+    <label>
+        <h3>Username
+        <textarea name = 'username' value = "%(username)s">
+        </textarea> 
+         <div style ="color: red">%(username_error)s</div>
+        </h3>
+    </label>
+    <label>
+        <h3>Password
+        <textarea name = 'password' type = "password">
+        </textarea> 
+        <div style ="color: red">%(password_error)s</div>
+        </h3>
+    </label>
+    <label>
+        <h3>Verify Password
+        <textarea name = 'verify' type = "password">
+        </textarea> 
+        <div style ="color: red">%(verification_error)s</div>
+        </h3>
+    </label>
+    <label>
+        <h3>Email (optional)
+        <textarea name = 'email' value ="%(email)s">
+        </textarea> 
+         <div style ="color: red">%(email_error)s</div>
+        </h3>
+    </label>
+    <br>
+    <input type = submit>
+
+</form>
+"""
+
+header = "<h1>Signup </h1><br>"
+
+user_re = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+password_re = re.compile(r"^.{3,20}$")
+email_re = re.compile(r"^[\S]+@[\S]+.[\S]+$")
+
+def valid_username(username):
+    return user_re.match(username) 
+
+def valid_password(password):
+    return password_re.match(password)
+
+def valid_email(email):
+    return email_re.match(email)
+
+class MainHandler(webapp2.RequestHandler):
+    def write_form(self, username_error="", password_error="", verification_error="", 
+                    email_error="", username="",email=""):
+        self.response.out.write(header + form %{"username_error": username_error,
+        "password_error": password_error, "verification_error": verification_error, 
+        "email_error":email_error, "username":username, "email":email})
+    
+    def get(self):
+        self.write_form()
+
+    def post(self):
+        user = self.request.get('username')
+        password = self.request.get('password')
+        verify = self.request.get('verify')
+        email = self.request.get('email')
+
+        if not valid_username(user):
+            username_error = "That's not a valid username"
+            self.write_form(username_error)
+        else:
+            self.response.write("Welcome, " + user +"!")
+       # elif not password or not valid_password(password):
+       #     password_error = "That wasn't a valid password"
+       #     self.response.write(form)
+       # elif password != verify:
+       #     verification_error = "Your passwords didn't match"
+       # elif not valid_email(email):
+       #     email_error = "That's not a vaild email"
+
+app = webapp2.WSGIApplication([
+    ('/', MainHandler)
+], debug=True)
+
+
